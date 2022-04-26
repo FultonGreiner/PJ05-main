9989435ecda8c9af49641d12b6bb3237cdc27346
Chingyan Huang <102830147+huan1730@users.noreply.github.com>
2022-04-26 10:11:30 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -10,7 +10,11 @@ import java.io.*;
  * or have them create a whole new account. Functionalities differ
  * between user types.
  *
- *
+ * @author Chingyan Huang
+ * @author Charles Greiner
+ * @author Akash Sridhar
+ * @author Benn Sesante
+ * @version 5-2-2022
  */
 
 public class Menu {
@@ -18,36 +22,31 @@ public class Menu {
 
     public static void main(String[] args) {
         try {
-
-            boolean continueOk = true;
             Menu menu = new Menu();
-            do {
-                String answer = "";
-                JOptionPane.showMessageDialog(null, "Welcome", "Main menu", JOptionPane.INFORMATION_MESSAGE);
 
-                do {
-                    String[] initialMenu = {"Login", "Create new account", "Exit"};
-                    answer = (String) JOptionPane.showInputDialog(null, "Choose one", "Main menu", JOptionPane.PLAIN_MESSAGE, null, initialMenu, null);
+            String answer = "";
+            JOptionPane.showMessageDialog(null, "Welcome", "Main menu", JOptionPane.INFORMATION_MESSAGE);
 
-                    if (answer.equals("Login")) {
-                        continueOk = menu.login();
-                        if (continueOk == false) {
-                            JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
-                            return;
-                        }
-                    } else if (answer.equals("Create new account")) {
-                        continueOk = menu.create();
-                        if (continueOk == false) {
-                            JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
-                            return;
-                        }
-                    } else if (answer.equals("Exit")) {
-                        JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
-                        return;
-                    }
-                } while (answer == null);
 
-            } while (continueOk == true);
+            String[] initialMenu = {"Login", "Create new account", "Exit"};
+            answer = (String) JOptionPane.showInputDialog(null, "Choose one", "Main menu", JOptionPane.PLAIN_MESSAGE, null, initialMenu, null);
+
+            if (answer == String.valueOf(JOptionPane.CANCEL_OPTION) || answer == String.valueOf(JOptionPane.CLOSED_OPTION)) {
+                JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                return;
+
+            }
+            if (answer.equals("Login")) {
+                menu.login();
+            } else if (answer.equals("Create new account")) {
+                menu.create();
+            } else if (answer.equals("Exit")) {
+                JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                return;
+            }
+
+
         } catch (NullPointerException e) {
             JOptionPane.showMessageDialog(null, "Goodbye! We hope to see you again.", "Main menu", JOptionPane.PLAIN_MESSAGE);
             return;
@@ -56,58 +55,57 @@ public class Menu {
 
     /**
      * Login
-     *
+     * <p>
      * The user inputs a username and a password and the program checks if the
-     * inputs match known records.
+     * inputs match known records. If the user chooses the cancel or close buttons
+     * (which both return null), the program closes.
      *
-     * #NOTE: LARGELY UNTESTED. Both Login and Create have some bugs, but I haven't got
-     * around to completely fixing them yet. Proceed with caution.
-     *
-     * @return  returns a boolean indicating whether a matching user has been found.
+     * @author Chingyan Huang
      */
 
-    public boolean login() {
+    public void login() {
         try {
-            boolean userFound = false;
             String username = "";
             String password = "";
+            boolean userFound = false;
+
             do {
                 username = JOptionPane.showInputDialog(null, "Enter your username", "Main menu", JOptionPane.QUESTION_MESSAGE);
-                if (username == String.valueOf(JOptionPane.CANCEL_OPTION)) {
-                    userFound = false;
-                    return userFound;
+                if (username == null) {
+                    JOptionPane.showMessageDialog(null, "Exiting...", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                    return;
                 }
-                if (username == String.valueOf(JOptionPane.CLOSED_OPTION)) {
-                    userFound = false;
-                    return userFound;
-                }
-                if (username == null || username.equals("")) {
-                    JOptionPane.showMessageDialog(null, "Username cannot be null!", "Main menu", JOptionPane.ERROR_MESSAGE);
-                }
-            } while (username == null || username.equals(""));
-            do {
+
                 password = JOptionPane.showInputDialog(null, "Enter your password", "Main menu", JOptionPane.QUESTION_MESSAGE);
-                if (password == String.valueOf(JOptionPane.CANCEL_OPTION)) {
+                if (password == null) {
+                    JOptionPane.showMessageDialog(null, "Exiting...", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                    return;
+                }
+                FileInput in = new FileInput();
+                FileEdit edit = new FileEdit();
+                if (in.readStudent(username, password) != null) {
                     userFound = true;
-                    return userFound;
+                    Student student = in.readStudent(username, password);
+                    FileOutput out = new FileOutput();
+                    StudentMenu studentMenu = new StudentMenu(student, in, out, edit);
+                    JOptionPane.showMessageDialog(null, "Login successful!", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                } else if (in.readTeacher(username, password) != null) {
+                    userFound = true;
+                    Teacher teacher = in.readTeacher(username, password);
+                    FileOutput out = new FileOutput();
+                    TeacherMenu teacherMenu = new TeacherMenu(teacher, in, out, edit);
+                    JOptionPane.showMessageDialog(null, "Login successful!", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                } else if (!edit.checkPassword(username, password, "TeacherData.txt") || !edit.checkPassword(username, password, "StudentData.txt")) {
+                    throw new IncorrectPasswordException();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Unable to find user.", "Main menu", JOptionPane.ERROR_MESSAGE);
                 }
-                if (username == String.valueOf(JOptionPane.CLOSED_OPTION)) {
-                    userFound = false;
-                    return userFound;
-                }
-                if (password == null || password.equals("")) {
-                    JOptionPane.showMessageDialog(null, "Password cannot be null!", "Main menu", JOptionPane.ERROR_MESSAGE);
-                }
-            } while (password == null || password.equals(""));
-            //add a check here to see if username and password match known records; if not, show error screen
-            return userFound;
-
-            //check student/teacher records!
-
-
+            } while (!userFound);
+        } catch (IncorrectPasswordException e) {
+            JOptionPane.showMessageDialog(null, "Invalid password", "Main menu", JOptionPane.ERROR_MESSAGE);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error", "Main menu", JOptionPane.ERROR_MESSAGE);
-            return false;
+            return;
         }
     }
 
@@ -115,83 +113,63 @@ public class Menu {
      * Create
      * A user chooses if they are a student or teacher, enters their information, and creates
      * an account if the username has not already been taken.
+     * <p>
+     * note: showInputDialog does not distinguish between the cancel/close buttons. Both
+     * are considered null values. Therefore, if the user chooses either of these two options,
+     * they just exit the program.
      *
-     * @return  returns a boolean indicating whether or not the user wants to continue logging in
-     * (basically whether or not they hit the cancel/x button)
+     * @author Chingyan Huang
      */
-    public boolean create() {
+    public void create() {
         try {
             String[] type = {"student", "teacher"};
-            boolean continueOk;
             String user = "";
             String username = "";
             String password = "";
             String name = "";
-            do {
-                user = (String) JOptionPane.showInputDialog(null, "Are you a teacher or a student?", "Main menu", JOptionPane.PLAIN_MESSAGE, null, type, null);
-                if (user == String.valueOf(JOptionPane.CANCEL_OPTION)) {
-                    continueOk = true;
-                    return continueOk;
-                }
-                if (user == String.valueOf(JOptionPane.CLOSED_OPTION)) {
-                    continueOk = false;
-                    return continueOk;
-                }
-                if (user == null) {
-                    JOptionPane.showMessageDialog(null, "Field cannot be null!", "Main menu", JOptionPane.ERROR_MESSAGE);
-                }
 
-            } while (user == null);
-            do {
-                name = JOptionPane.showInputDialog(null, "What is your name?", "Main menu", JOptionPane.QUESTION_MESSAGE);
-                if (name == String.valueOf(JOptionPane.CANCEL_OPTION)) {
-                    continueOk = true;
-                    return continueOk;
-                }
-                if (name == String.valueOf(JOptionPane.CLOSED_OPTION)) {
-                    continueOk = false;
-                    return continueOk;
-                }
-                if (name == null) {
-                    JOptionPane.showMessageDialog(null, "Field cannot be null!", "Main menu", JOptionPane.ERROR_MESSAGE);
-                }
-            } while (name == null);
+            user = (String) JOptionPane.showInputDialog(null, "Are you a teacher or a student?", "Main menu", JOptionPane.PLAIN_MESSAGE, null, type, null);
+            if (user == null) {
+                JOptionPane.showMessageDialog(null, "Exiting...", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                return;
+            }
+
+
+            name = JOptionPane.showInputDialog(null, "What is your name?", "Main menu", JOptionPane.QUESTION_MESSAGE);
+            if (name == null) {
+                JOptionPane.showMessageDialog(null, "Exiting...", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                return;
+            }
+
+            FileEdit edit = new FileEdit();
             do {
                 username = JOptionPane.showInputDialog(null, "Enter a username", "Main menu", JOptionPane.QUESTION_MESSAGE);
-                if (username == String.valueOf(JOptionPane.CANCEL_OPTION)) {
-                    continueOk = true;
-                    return continueOk;
-                }
-                if (username == String.valueOf(JOptionPane.CLOSED_OPTION)) {
-                    continueOk = false;
-                    return continueOk;
-                }
                 if (username == null) {
-                    JOptionPane.showMessageDialog(null, "Please input a username", "Main menu", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Exiting...", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                    return;
                 }
+                if (edit.checkUsername(username, username + ".txt")) {
+                    JOptionPane.showMessageDialog(null, "That username has been taken", "Main menu", JOptionPane.ERROR_MESSAGE);
+                }
+            } while (edit.checkUsername(username, username + ".txt"));
 
-                //input check for username
-            } while (username == null);
-            do {
-                password = JOptionPane.showInputDialog(null, "Enter a password", "Main menu", JOptionPane.QUESTION_MESSAGE);
-                if (password == String.valueOf(JOptionPane.CANCEL_OPTION)) {
-                    continueOk = true;
-                    return continueOk;
-                }
-                if (password == String.valueOf(JOptionPane.CLOSED_OPTION)) {
-                    continueOk = false;
-                    return continueOk;
-                }
-                if (password == null) {
-                    JOptionPane.showMessageDialog(null, "Please input a password", "Main menu", JOptionPane.ERROR_MESSAGE);
-                }
-                //check for password
-            } while (password == null);
-            return true;
+            password = JOptionPane.showInputDialog(null, "Enter a password", "Main menu", JOptionPane.QUESTION_MESSAGE);
+            if (password == null) {
+                JOptionPane.showMessageDialog(null, "Exiting...", "Main menu", JOptionPane.PLAIN_MESSAGE);
+                return;
+            }
+            FileOutput out = new FileOutput();
+            if (user.equals("student")) {
+                Student student = new Student(name, username, password);
+                out.writeStudent(student);
+            } else if (user.equals("teacher")) {
+                Teacher teacher = new Teacher(name, username, password);
+                out.writeTeacher(teacher);
+            }
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error, please reconnect.", "Main menu", JOptionPane.ERROR_MESSAGE);
-            return false;
+            return;
         }
     }
-
 }
